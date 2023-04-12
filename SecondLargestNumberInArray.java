e0b7fe5d88f972112eb037af415a1c8a310aed24
raju dey <rajudey432@gmail.com>
2023-04-12 23:10:10 +0530

    added: second largest number in array

---
+++ SecondLargestNumberInArray.java
@@ -0,0 +1,27 @@
+package array;
+
+import java.util.*;
+
+public class SecondLargestNumberInArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int largest = arr[0];
+        int secondLargest = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+        System.out.println("The second largest number in the array is " + secondLargest);
+    }
+}
